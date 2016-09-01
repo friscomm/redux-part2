9f0f5c22e1a7e53e2b02261f8e9a76dcecb0f23b
Frisco Miranda <friscomiranda@gmail.com>
2016-08-31 21:19:49 -0700

    add reducers and store

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,6 +1,10 @@
 import 'babel-polyfill';
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import allReducers from './reducers';
+
+const store = createStore(allReducers);
 
 
 ReactDOM.render(
---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,53 @@
+export default function() {
+  return [
+    {
+      id: 1,
+      first: "Frisco",
+      last: "Miranda",
+      age: 26,
+      description: "He's amazing, and a great dancer. Much better than anybody he lives with. Also what video game skills. I wouldn't suggest challenging him to any games unless you're a masochist.",
+      thumbnail: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/5/005/09e/3a9/26d716b.jpg"
+    },
+    {
+      id: 2,
+      first: "Ruby",
+      last: "Miranda",
+      age: 24,
+      description: "She's aight, and an okay dancer. She could learn a thing or two from that Frisco guy though. She's preeeetty obsessed with video games. Recent calculations find that she spends about 20% of her day playing candy crush. What a video game obsession! In spite of her video game obsession her husband is very patient and supportive, what a great sounding guy.",
+      thumbnail: "http://universalwebsites.theknot.com.s3.amazonaws.com/UniversalWebsites/PageMainImage/5516448283579415/3152562/45855416/med/BDP_Engagement_SantaBarbara_FriscoRuby-222.jpg"
+    },
+    {
+      id: 3,
+      first: "Kenta",
+      last: "Ishii",
+      age: 25,
+      description: "He's devoted himself to trying to surpass Frisco in various measurements of physical strength. Clearly, that's proven to be a huge waste of his time and effort.",
+      thumbnail: "http://www.cdc.gov/phap/images/associates-talk/phap4.jpg"
+    },
+    {
+      id: 4,
+      first: "Brad",
+      last: "Feest",
+      age: 25,
+      description: "He's pretty into a certain poopface. She isn't the best, but somehow magical fairies convinced him to think so. His friends have gotten over the initial confusion, and are now very supportive of their relationship. Except for Frisco, he's still holding out hope that Bradley will choose him over poopface.",
+      thumbnail: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/005/007/38b/2461d56.jpg"
+    },
+    {
+      id: 5,
+      first: "Brianna",
+      last: "Chang",
+      age: 26,
+      description: "She's really into food and organizing things.",
+      thumbnail: "http://seaver.pepperdine.edu/naturalscience/images/students/outstanding/images/2012/tsuyukioutstandingnutritionretouchedwebjpg.jpg"
+    },
+    {
+      id: 6,
+      first: "David",
+      last: "Chang",
+      age: 25,
+      description: "He's really into Brianna.",
+      thumbnail: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAljAAAAJGYwOTNhNzYyLTc4NmEtNDI0Yi04OWFjLTkxMTk4NmZkNTY2NQ.jpg"
+    }
+
+  ]
+}
