65826154d3d1c7366f29b70dfdca117bb82656a2
Frisco Miranda <friscomiranda@gmail.com>
2016-09-02 12:47:44 -0700

    add provider and store to main app

--- dev/js/index.js
+++ dev/js/index.js
@@ -2,12 +2,16 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import allReducers from './reducers';
+import App from './components/app.js';
 
 const store = createStore(allReducers);
 
 
 ReactDOM.render(
-    <h1>Hi friend</h1>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
     document.getElementById('root')
 );
