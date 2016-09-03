25ef9a739c2a01872ce4da61a5b6824b6c6a1ff8
Frisco Miranda <friscomiranda@gmail.com>
2016-09-02 21:52:56 -0700

    add actions to track user activities on the page

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import UserList from '../containers/user-list.js';
 require('../../scss/style.scss');
 
 const App = () => (
     <div>
         <h2>User List</h2>
+        <UserList />
         <hr />
         <h2>User Details</h2>
     </div>
 );
+
 
 export default App;
